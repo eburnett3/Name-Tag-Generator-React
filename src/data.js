78b4ebce8137f6665db01a5d856d7a877b57d8e3
Eric <eburnett32@gmail.com>
2023-08-04 23:25:55 -0500

    changed from functional to stateful

--- src/data.js
+++
@@ -1,7 +0,0 @@
-export const names = [
-  { person: "Eric", id: "001" },
-  { person: "Tori", id: "002" },
-  { person: "Ghost", id: "003" },
-  { person: "Lacey", id: "004" },
-  { person: "Tito", id: "005" },
-];
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import NameTag from "./NameTag";
-import { names } from "./data.js";
+import NameTag from "./NameTag.js";
 import "./styles.css";
 
-const renderNameTag = (name) => <NameTag name={name.person} key={name.id} />;
+const renderNameTag = (name) => <NameTag name={name} key={name} />;
 
-const App = () => {
-  const NameTagElements = names.map(renderNameTag);
-  return (
-    <div className="App">
-      <h1>Name Tag Generator</h1>
-      {NameTagElements}
-    </div>
-  );
-};
+class App extends Component {
+  state = { names: ["Eric", "Tori", "Ghost", "Lacy", "Tito"] };
+  render() {
+    const NameTagElements = this.state.names.map(renderNameTag);
+    return (
+      <div className="App">
+        <h1>Name Tag Generator</h1>
+        {NameTagElements}
+      </div>
+    );
+  }
+}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
