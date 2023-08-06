ffe8a52ab44d2cd636e899c3e03b3061e1563393
Eric <eburnett32@gmail.com>
2023-08-05 23:24:45 -0500

    updating code to class components

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import NameTagList from "./NameTagList.js";
+
+class App extends Component {
+  state = {
+    names: ["Eric", "Tori", "Ghost", "Lacey", "Tito"],
+  };
+  removeName = (clickedIndex) => {
+    // to learn how the .filter method works, check out https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+    const filterCallback = (_, index) => index !== clickedIndex;
+    const newNames = this.state.names.filter(filterCallback);
+    this.setState({ names: newNames });
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Name Tag Generator</h1>
+        <NameTagList names={this.state.names} removeName={this.removeName} />
+      </div>
+    );
+  }
+}
+
+export default App;
